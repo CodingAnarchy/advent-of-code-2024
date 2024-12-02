f7b151a2a53e1885cf45d3558a96b565cd37cfec
Matt Tanous <matthew.tanous@shopify.com>
2024-12-02 17:43:19 -0500

    Clippy fixes

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,20 +1,20 @@
 advent_of_code::solution!(2);
 
-fn is_report_safe(report: &Vec<u32>) -> bool {
+fn is_report_safe(report: &[u32]) -> bool {
     if report.is_sorted() {
         if report.windows(2).all(|w| {
             let window_diff = w[1] - w[0];
-            1 <= window_diff && window_diff <= 3
+            (1..=3).contains(&window_diff)
         }) {
             return true;
         }
-    } else if report.is_sorted_by(|a, b| b <= a) {
-        if report.windows(2).all(|w| {
+    } else if report.is_sorted_by(|a, b| b <= a)
+        && report.windows(2).all(|w| {
             let window_diff = w[0] - w[1];
-            1 <= window_diff && window_diff <= 3
-        }) {
-            return true;
-        }
+            (1..=3).contains(&window_diff)
+        })
+    {
+        return true;
     }
 
     false
@@ -55,10 +55,9 @@ pub fn part_two(input: &str) -> Option<u32> {
 
             let report_copy = report.clone();
             for (idx, _n) in report.iter().enumerate() {
-                let index = idx.clone() as usize;
+                let index = idx;
                 let mut dampened_report = report_copy[0..index].to_vec();
-                dampened_report
-                    .append(&mut report_copy[index + 1..report_copy.len() as usize].to_vec());
+                dampened_report.append(&mut report_copy[index + 1..report_copy.len()].to_vec());
                 if is_report_safe(&dampened_report) {
                     return Some(true);
                 }
